094409e36d770ed45a8fa6b5cad4578498c9f49c
Yuiki <hayahayahaya19941@gmail.com>
2015-08-06 15:33:53 +0900

    compworkpage

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ SampleApp::Application.routes.draw do
   match '/request',    to: 'static_pages#request',    via: 'get' 
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/temp1', to: 'static_pages#temp1', via: 'get'
+  match '/work', to: 'static_pages#work', via: 'get'
   match '/temp2', to: 'static_pages#temp2', via: 'get'
   match '/temp3', to: 'static_pages#temp3', via: 'get'
   match '/temp4', to: 'static_pages#temp4', via: 'get'
